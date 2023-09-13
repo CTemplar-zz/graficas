5fdb557871bd04feb4095160b1e0999d8ba2563e
Ivar Zambrana <ivar.zambrana@gmail.com>
2023-09-13 12:03:16 -0400

    Updates

---
+++ js/about.a08a72ee.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgraficas"]=self["webpackChunkgraficas"]||[]).push([[443],{1797:function(n,s,t){t.r(s),t.d(s,{default:function(){return c}});var u=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,s=n._self._c;return s("div",{staticClass:"about"},[s("h1",[n._v("This is an about page")])])}],r=t(1001),e={},i=(0,r.Z)(e,u,a,!1,null,null,null),c=i.exports}}]);
+//# sourceMappingURL=about.a08a72ee.js.map
